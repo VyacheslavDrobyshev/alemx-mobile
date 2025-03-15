072694cb3786ae39f289c55e0bf04f951ad2ef41
Andriy Karpenko <andriy.karpenko@holovis.com>
2025-03-15 13:54:15 +0200

    - History component refactoring

--- src/features/wallet/components/HistoryTabContent/HistoryTabContent.tsx
+++ src/features/wallet/components/HistoryTabContent/HistoryTabContent.tsx
@@ -1,6 +1,6 @@
 import { AppTab } from '@app/components/AppTab/AppTab.tsx';
 import { AppView } from '@app/components';
-import { FlatList } from 'react-native';
+import { FlatList, ListRenderItem } from 'react-native';
 import {
   paginationLimit,
   TransactionType,
@@ -11,28 +11,74 @@ import {
   DepositTransaction,
   SwapTransaction,
   TransferTransaction,
+  UnionTransaction,
   WithdrawalTransaction,
 } from '@app/features/wallet/redux/types.ts';
 import { EmptyListPlaceholder } from '@app/features/wallet/components/EmptyListPlaceholder/EmptyListPlaceholder.tsx';
 import { WithdrawalItem } from '@app/features/wallet/components/HistoryTabContent/components/WithdrawalItem/WithdrawalItem.tsx';
 import { TransferItem } from '@app/features/wallet/components/HistoryTabContent/components/TransferItem/TransferItem.tsx';
 import { SwapItem } from '@app/features/wallet/components/HistoryTabContent/components/SwapItem/SwapItem.tsx';
-import { FC, useCallback } from 'react';
+import React, { FC, useCallback } from 'react';
 import { getTransactionsThunk } from '@app/features/wallet/redux/thunks.ts';
 import { useAppDispatch } from '@app/redux';
 import { useSelector } from 'react-redux';
-import { selectTransactionsByType } from '@app/features/wallet/redux/selectors.ts';
+import {
+  selectIsTransactionsLoading,
+  selectTransactionsByType,
+} from '@app/features/wallet/redux/selectors.ts';
 import { useAppTheme } from '@app/theme';
 import { transactionHistoryTabs } from '@app/features/wallet/screens/WalletDetails/constants.ts';
+import { AppActivityIndicator } from '@app/components/AppActivityIndicator/AppActivityIndicator.tsx';
 
-export const HistoryTabContent: FC<{ assetSymbol?: string }> = ({
+export type HistoryTabContentProps = {
+  assetSymbol?: string;
+  transactionType: TransactionType;
+  renderItem: ListRenderItem<UnionTransaction>;
+};
+
+export const HistoryList: FC<HistoryTabContentProps> = ({
   assetSymbol,
+  transactionType,
+  renderItem,
 }) => {
-  const dispatch = useAppDispatch();
-  const transactions = useSelector(selectTransactionsByType);
   const {
     cryptoCurrencyList: { contentContainerStyle },
   } = useAppTheme();
+  const transactions = useSelector(selectTransactionsByType);
+  const isTransactionsLoading = useSelector(selectIsTransactionsLoading);
+
+  const filteredFunction = <T extends { crypto_asset: CryptoAssetTransaction }>(
+    el: T,
+  ): boolean => {
+    return assetSymbol ? el.crypto_asset.symbol === assetSymbol : true;
+  };
+
+  return (
+    <AppView flex={1} justifyContent="center" alignItems="center">
+      {isTransactionsLoading ? (
+        <AppActivityIndicator absoluteFill />
+      ) : (
+        <FlatList
+          contentContainerStyle={contentContainerStyle}
+          data={transactions[transactionType].filter(filteredFunction)}
+          renderItem={renderItem}
+          ListEmptyComponent={
+            <EmptyListPlaceholder
+              title={
+                'You have no deposits yet. All your deposits will be displayed here.'
+              }
+            />
+          }
+        />
+      )}
+    </AppView>
+  );
+};
+
+export const HistoryTabContent: FC<
+  Pick<HistoryTabContentProps, 'assetSymbol'>
+> = ({ assetSymbol }) => {
+  const dispatch = useAppDispatch();
 
   const onTabChange = useCallback(
     (index: number) => {
@@ -80,78 +126,48 @@ export const HistoryTabContent: FC<{ assetSymbol?: string }> = ({
     [dispatch],
   );
 
-  const filteredFunction = <T extends { crypto_asset: CryptoAssetTransaction }>(
-    el: T,
-  ): boolean => {
-    return assetSymbol ? el.crypto_asset.symbol === assetSymbol : true;
-  };
+  const renderDepositItem = useCallback<ListRenderItem<UnionTransaction>>(
+    ({ item }) => <DepositItem item={item as DepositTransaction} />,
+    [],
+  );
+
+  const renderWithdrawItem = useCallback<ListRenderItem<UnionTransaction>>(
+    ({ item }) => <WithdrawalItem item={item as WithdrawalTransaction} />,
+    [],
+  );
+
+  const renderTransferItem = useCallback<ListRenderItem<UnionTransaction>>(
+    ({ item }) => <TransferItem item={item as TransferTransaction} />,
+    [],
+  );
+
+  const renderSwapItem = useCallback<ListRenderItem<UnionTransaction>>(
+    ({ item }) => <SwapItem item={item as SwapTransaction} />,
+    [],
+  );
 
   return (
     <AppTab onTabChange={onTabChange} tabs={transactionHistoryTabs}>
-      <AppView flex={1} justifyContent="center" alignItems="center">
-        <FlatList
-          contentContainerStyle={contentContainerStyle}
-          data={transactions[TransactionType.Deposit].filter(filteredFunction)}
-          renderItem={({ item }) => (
-            <DepositItem item={item as DepositTransaction} />
-          )}
-          ListEmptyComponent={
-            <EmptyListPlaceholder
-              title={
-                'You have no deposits yet. All your deposits will be displayed here.'
-              }
-            />
-          }
-        />
-      </AppView>
-      <AppView flex={1} justifyContent="center" alignItems="center">
-        <FlatList
-          contentContainerStyle={contentContainerStyle}
-          data={transactions[TransactionType.Withdrawal].filter(
-            filteredFunction,
-          )}
-          renderItem={({ item }) => (
-            <WithdrawalItem item={item as WithdrawalTransaction} />
-          )}
-          ListEmptyComponent={
-            <EmptyListPlaceholder
-              title={
-                'You have no withdrawals yet. All your withdrawals will be displayed here.'
-              }
-            />
-          }
-        />
-      </AppView>
-      <AppView flex={1} justifyContent="center" alignItems="center">
-        <FlatList
-          contentContainerStyle={contentContainerStyle}
-          data={transactions[TransactionType.Transfer].filter(filteredFunction)}
-          renderItem={({ item }) => (
-            <TransferItem item={item as TransferTransaction} />
-          )}
-          ListEmptyComponent={
-            <EmptyListPlaceholder
-              title={
-                'You have no transfers yet. All your transfers will be displayed here.'
-              }
-            />
-          }
-        />
-      </AppView>
-      <AppView flex={1} justifyContent="center" alignItems="center">
-        <FlatList
-          contentContainerStyle={contentContainerStyle}
-          data={transactions[TransactionType.Base].filter(filteredFunction)}
-          renderItem={({ item }) => <SwapItem item={item as SwapTransaction} />}
-          ListEmptyComponent={
-            <EmptyListPlaceholder
-              title={
-                'You have no exchanges yet. All your exchanges will be displayed here.'
-              }
-            />
-          }
-        />
-      </AppView>
+      <HistoryList
+        renderItem={renderDepositItem}
+        assetSymbol={assetSymbol}
+        transactionType={TransactionType.Deposit}
+      />
+      <HistoryList
+        renderItem={renderWithdrawItem}
+        assetSymbol={assetSymbol}
+        transactionType={TransactionType.Withdrawal}
+      />
+      <HistoryList
+        renderItem={renderTransferItem}
+        assetSymbol={assetSymbol}
+        transactionType={TransactionType.Transfer}
+      />
+      <HistoryList
+        renderItem={renderSwapItem}
+        assetSymbol={assetSymbol}
+        transactionType={TransactionType.Base}
+      />
     </AppTab>
   );
 };
