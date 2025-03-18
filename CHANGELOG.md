ecfad7350e4dc0b1e9daa961cdf7178c09a680fc
Andriy Karpenko <andriy.karpenko@holovis.com>
2025-03-18 13:01:24 +0200

    - fixed logic for deposit flow

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,8 +4,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## [Unreleased]
+## [0.0.1]
 ### Added
 - Implemented - Deposit
 - Implemented - Withdrawal
 - Implemented - Transfer
+- Changed logic related to deposit flow
--- src/features/wallet/screens/Deposit/DepositScreen.tsx
+++ src/features/wallet/screens/Deposit/DepositScreen.tsx
@@ -1,16 +1,10 @@
-import { FC, useCallback, useEffect, useState } from 'react';
+import { FC, useCallback, useEffect } from 'react';
 import { AppScreen } from '@app/components';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { WalletParamList } from '@app/features/wallet/navigation/types';
 import { WalletRoute } from '@app/features/wallet/navigation/constants';
-import { isThunkPayload, useAppDispatch } from '@app/redux';
-import {
-  createUserWalletsThunk,
-  getDepositWalletsThunk,
-} from '@app/features/wallet/redux/thunks';
-import { useSelector } from 'react-redux';
-import { selectUserWallets } from '@app/features/wallet/redux/selectors';
-import { useAppToast } from '@app/components/AppToast/useAppToast';
+import { useAppDispatch } from '@app/redux';
+import { getDepositWalletsThunk } from '@app/features/wallet/redux/thunks';
 import {
   ModifiedWallet,
   WalletsList,
@@ -18,31 +12,13 @@ import {
 
 export const DepositScreen: FC = () => {
   const { navigate } = useNavigation<NavigationProp<WalletParamList>>();
-  const { showError } = useAppToast();
   const dispatch = useAppDispatch();
-  const wallets = useSelector(selectUserWallets);
-  const [isLoading, setIsLoading] = useState(false);
 
   const onPress = useCallback(
-    async (item: ModifiedWallet) => {
-      const chosenWallet = wallets?.find(el => el.cryptoAsset.id === item.id);
-      if (chosenWallet) {
-        navigate(WalletRoute.DepositDetails, { item: chosenWallet });
-      } else {
-        setIsLoading(true);
-        const { payload } = await dispatch(
-          createUserWalletsThunk({ assetsIds: [item.id] }),
-        );
-        if (isThunkPayload(payload)) {
-          const newWallet = payload?.find(el => el.cryptoAsset.id === item.id);
-          navigate(WalletRoute.DepositDetails, { item: newWallet });
-        } else {
-          showError(payload ?? '');
-        }
-        setIsLoading(false);
-      }
+    (item: ModifiedWallet) => {
+      navigate(WalletRoute.DepositDetails, { item });
     },
-    [dispatch, navigate, showError, wallets],
+    [navigate],
   );
 
   useEffect(() => {
@@ -50,7 +26,7 @@ export const DepositScreen: FC = () => {
   }, [dispatch]);
 
   return (
-    <AppScreen isLoading={isLoading} title="Deposit" noScroll>
+    <AppScreen title="Deposit" noScroll>
       <WalletsList showNetwork isDeposit onPress={onPress} />
     </AppScreen>
   );
